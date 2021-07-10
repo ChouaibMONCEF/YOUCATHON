6a266f05afd5c797eb7d1f5a9d2a6761d16dac49
MohssineElAttari <14mohssine@gmail.com>
2021-07-10 11:26:44 +0100

    first commit

---
+++ BACKEND/controller/ApiUser.php
@@ -0,0 +1,106 @@
+<?php
+
+
+class ApiUser
+{
+
+    public function index()
+    {
+        // header('index:')
+        echo ("this an index method");
+    }
+
+    public function getUserInfos($id)
+    {
+        // headers
+        header('Access-Control-Allow-Origin:*');
+        header('Content-Type: application/json');
+
+        // instantiate Database
+        $database = new Database();
+        $db = $database->connect();
+
+        // instantiate User object
+        $user = new Users($db);
+
+        // user informations
+        $user_array = array();
+
+        // get user id
+        $user->userId = $id;
+
+        // user read query
+        $result = $user->read_single();
+
+        if ($result) {
+            $row = $result->fetch(PDO::FETCH_ASSOC);
+            extract($row);
+
+
+            $user_array = array(
+                'userId' => $userId,
+                'userFirstName' => $userFirstName,
+                'userLastName' => $userLastName,
+                'userCIN' => $userCIN,
+                'userEmail' => $userEmail,
+                'userPassword' => $userPassword,
+            );
+
+            echo json_encode(
+                array($user_array)
+            );
+        } else {
+            echo json_encode(
+                array('message' => 'user not inserted')
+            );
+        }
+    }
+
+
+    public function read()
+    {
+
+        // headers
+        header('Access-Control-Allow-Origin:*');
+        header('Content-Type: application/json');
+
+        // instantiate Database
+        $database = new Database();
+        $db = $database->connect();
+
+        // instantiate User object
+        $user = new Users($db);
+
+        // user read query
+        $result = $user->read();
+
+        // get row count 
+        $num = $result->rowCount();
+
+        // check if there is a user 
+        if ($num > 0) {
+            // user array
+            $users_arr = array();
+            $users_arr['data'] = array();
+
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+                $user_arr = array(
+                    'userId' => $userId,
+                    'userFirstName' => $userFirstName,
+                    'userLastName' => $userLastName,
+                    'userEmail' => $userEmail,
+                    'userCIN' => $userCIN
+                );
+                array_push($users_arr['data'], $user_arr);
+            }
+            // Turn to JSON & output
+            echo json_encode($users_arr);
+        } else {
+            // No Users
+            echo json_encode(
+                array('message' => 'No Users found')
+            );
+        }
+    }
+}
---
+++ BACKEND/model/Users.php
@@ -0,0 +1,49 @@
+<?php
+
+class Users
+{
+    // db stuff
+    private $conn;
+    private $table = 'users';
+
+    // users properties
+    public $userId;
+    public $userCIN;
+    public $userEmail;
+    public $userFirstName;
+    public $userLastName;
+    public $userPassword;
+
+    // constructor with db
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+
+    // get users informations 
+    public function read()
+    {
+        // $query = 'SELECT * FROM' . $this->table;
+        $query = ' SELECT * FROM ' . $this->table . ';';
+        // prepare statement
+        $stmt = $this->conn->prepare($query);
+        // execute statement
+        $stmt->execute();
+        return $stmt;
+    }
+
+    // get one user informations
+    public function read_single()
+    {
+        $query = 'SELECT * FROM users  where userId=:userId ';
+        // prepare statement
+        $stmt = $this->conn->prepare($query);
+        // bind user id
+        $stmt->bindParam(':userId', $this->userId);
+        // execute statement
+        $stmt->execute();
+        return $stmt;
+    }
+
+}
